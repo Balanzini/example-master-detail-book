51ed5b62bfed6b95e06f5df4c05f6b8617dd03bc
Balanzini <jl.balanzaperez@gmail.com>
2016-07-04 21:21:32 +0200

    firs commit

---
+++ app/src/main/java/com/jose/books/app/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.jose.books.app.base;
+
+import android.app.Activity;
+
+/**
+ * Created by jose on 4/07/16.
+ */
+public abstract class BaseActivity extends Activity {
+
+}
---
+++ app/src/main/java/com/jose/books/app/base/BasePresenter.java
@@ -0,0 +1,7 @@
+package com.jose.books.app.base;
+
+/**
+ * Created by jose on 4/07/16.
+ */
+public abstract class BasePresenter {
+}
